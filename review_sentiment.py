c2f71ec10d2fb130fb0fda48ecb39128bc02dd0d
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2025-11-18 17:41:49 -0500

    Added dataset and classifier classes

--- review_sentiment.py
+++ review_sentiment.py
@@ -178,3 +178,87 @@ class ReviewVectorizer(object):
         """
         return {'review_vocab': self.review_vocab.to_serializable(),
                 'rating_vocab': self.rating_vocab.to_serializable()}
+    
+class ReviewDataset(Dataset):
+    def __init__(self, review_df, vectorizer):
+        self.review_df = review_df
+        self._vectorizer = vectorizer
+        
+        self.train_df = self.review_df[self.review_df.split == 'train']
+        self.train_size = len(self.train_df)
+
+        self.val_df = self.review_df[self.review_df.split == 'val']
+        self.val_size = len(self.val_df)
+
+        self.test_df = self.review_df[self.review_df.split == ' test']
+        self.test_size = len(self.test_df)
+
+        self._lookup_dict = {'train': (self.train_df, self.train_size),
+                             'val': (self.val_df, self.val_size),
+                             'test': (self.test_df, self.test_size)}
+        
+        self.set_split('train')
+
+    @classmethod
+    def load_dataset_make_vectorizer(cls, review_csv):
+        """Load dataset and make a new vectorizer"""
+        review_df = pd.read_csv(review_csv)
+        train_review_df = review_df[review_df.split=="train"]
+        return cls(review_df, ReviewVectorizer.from_df(train_review_df))
+    
+    @classmethod
+    def load_data_load_vectorizre(cls, review_csv, vectorizer_filepath):
+        """Load dataset and load vectorizer"""
+        review_df = pd.read_csv(review_csv)
+        vectorizer = cls.load_vectorizer_only(vectorizer_filepath)
+        return cls(review_df, vectorizer)
+    
+    @staticmethod
+    def load_vectorizer_only(vectorizer_filepath):
+        with open(vectorizer_filepath) as fp:
+            return ReviewVectorizer.from_serializable(json.load(fp))
+
+    def save_vectorizer(self, vectorizer_filepath):
+        """saves the vectorizer to disk using json
+        
+        Args:
+            vectorizer_filepath (str): the location to save the vectorizer
+        """
+        with open(vectorizer_filepath, "w") as fp:
+            json.dump(self._vectorizer.to_serializable(), fp)
+
+    def get_vectorizer(self):
+        return self._vectorizer
+    
+    def set_split(self, split = 'train'):
+        """
+        Args:
+            split (str): one of 'train', 'val' or 'test'
+        """
+        self._target_split = split
+        self._target_df, self._target_size = self._lookup_dict[split]
+
+    def __len__(self):
+        return self._target_size
+    
+    def __getitem__(self, index):
+        row = self._target_df.iloc[index]
+
+        review_vector = self._vectorizer.vectorize(row.review)
+        
+        rating_index = self._vectorizer.rating_vocab.lookup_token(row.rating)
+        return {'x_data': review_vector,
+                'y_target': rating_index}
+
+    def get_num_batches(self, batch_size):
+        return len(self)//batch_size
+    
+def generate_batches(dataset, batch_size, shuffle=True, drop_last=True, device='cpu'):
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, 
+                            drop_last=drop_last)
+    
+    for data_dict in dataloader:
+        out_data_dict = {}
+        for name, tensor in data_dict.items():
+            out_data_dict[name] = data_dict[name].to(device)
+        yield out_data_dict
